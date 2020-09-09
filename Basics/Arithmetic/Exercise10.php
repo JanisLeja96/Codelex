fe48e1456042ebd222740ae01b1480809b6f87b3
JanisLeja96 <janis.leja96@gmail.com>
2020-09-09 14:04:14 +0300

    Initial commit

---
+++ Basics/Arithmetic/Exercise10.php
@@ -0,0 +1,67 @@
+<?php
+
+class Geometry {
+
+    public static function circleArea() {
+        $radius = readline('Enter radius: ');
+        if ($radius < 0) {
+            throw new Error('Negative values cannot be used');
+        }
+        echo "Area is " . pi() * $radius * 2;
+    }
+
+    public static function rectangleArea() {
+        $length = readline('Enter length: ');
+        if ($length < 0) {
+            throw new Error('Length cannot be negative');
+        }
+        $width = readline('Enter width: ');
+        if ($width < 0) {
+            throw new Error("Width cannot be negative");
+        }
+        echo "Area is " . $length * $width;
+    }
+
+    public static function triangleArea() {
+        $base = readline("Enter base: ");
+        if ($base < 0) {
+            throw new Error('Base cannot be negative');
+        }
+        $height = readline('Enter height');
+        if ($height < 0) {
+            throw new Error('Height cannot be negative');
+        }
+        echo "Area is " . $base * $height * 0.5;
+    }
+}
+
+echo "Geometry Calculator\n";
+echo "1. Calculate the Area of a Circle\n";
+echo "2. Calculate the Area of a Rectangle\n";
+echo "3. Calculate the Area of a Triangle\n";
+echo "4. Quit\n";
+echo "Enter your choice (1-4) : \n";
+
+$choice = readline();
+
+if ($choice < 1 || $choice > 4) {
+    throw new Error('Invalid number entered');
+}
+
+switch ($choice) {
+    case 1:
+        Geometry::circleArea();
+        break;
+
+    case 2:
+        Geometry::rectangleArea();
+        break;
+
+    case 3:
+        Geometry::triangleArea();
+        break;
+
+    case 4:
+        exit(0);
+}
+
--- Basics/Arithmetic/Exercise9.php
+++ Basics/Arithmetic/Exercise9.php
@@ -1,1 +1,16 @@
 <?php
+
+function calculateBMI($weight, $height) {
+    $BMI = $weight / (pow($height, 2));
+    echo "Your BMI is {$BMI}\n";
+
+    if ($BMI > 18.5 && $BMI < 25) {
+        echo 'You have optimal weight!';
+    } else if ($BMI <= 18.5) {
+        echo 'You are underweight';
+    } else if ($BMI >= 25) {
+        echo 'You are overweight';
+    }
+}
+
+calculateBMI($argv[1], $argv[2]);
