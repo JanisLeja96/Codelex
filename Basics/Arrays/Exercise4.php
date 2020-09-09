809d4aa9661b1d90be26aea179c021492d17d6d5
JanisLeja96 <janis.leja96@gmail.com>
2020-09-09 14:12:26 +0300

    Initial commit

--- Basics/Arrays/Exercise4.php
+++ Basics/Arrays/Exercise4.php
@@ -1,1 +1,24 @@
 <?php
+
+$array1 = array();
+
+for ($i = 0; $i < 10; $i++) {
+    $array1[$i] = random_int(1,100);
+}
+
+$array2 = array();
+
+for ($i = 0; $i < 10; $i++) {
+    $array2[$i] = $array1[$i];
+}
+
+$array1[count($array1) - 1] = -7;
+
+echo 'Array 1: ';
+for($i = 0; $i < 10; $i++) {
+    echo "{$array1[$i]} ";
+}
+echo "\nArray 2: ";
+for($i = 0; $i < 10; $i++) {
+    echo "{$array2[$i]} ";
+}
